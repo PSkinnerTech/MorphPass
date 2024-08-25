8187ad0b71673f1bf282a88f62a8abc60a2bed06
PSkinnerTech <patrick@clgcorporation.com>
2024-08-25 10:52:48 -0500

    Baseline ERC721 Contract Completed

--- contracts/morphPass.sol
+++ contracts/morphPass.sol
@@ -3,8 +3,60 @@ pragma solidity ^0.8.19;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+contract MorphPass is ERC721 {
+    address public owner;
+    uint256 public totalInstances;
 
-contract morphPass {}
- 
+    struct Instance {
+        string name;
+        uint256 id;
+        uint256 cost;
+        uint256 tickets;
+        uint256 maxTickets;
+        string time;
+        string date;
+        string location;
+    }
+
+    mapping(uint256 => Instance) instances;  
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {
+        owner = msg.sender;
+    }
+
+    function list(
+        string memory _name,
+        uint256 _cost,
+        uint256 _maxTickets,
+        string memory _time,
+        string memory _date,
+        string memory _location 
+    ) public onlyOwner {
+
+        totalInstances++;
+
+        instances[totalInstances] = Instance(
+            _name,
+            totalInstances,
+            _cost,
+            100, // Initial tickets sold
+            _maxTickets,
+            _time,
+            _date,
+            _location
+        );
+    }
+
+    function getInstance(uint256 _id) public view returns (Instance memory) {
+        return instances[_id];
+    }
+
+}
--- test/morphPass.js
+++ test/morphPass.js
@@ -1,5 +1,66 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("morphPass", () => {
+const NAME = "MorphPass"
+const SYMBOL = "MP"
 
+const INSTANCE_NAME = "Morphin Time"
+const INSTANCE_COST = ethers.parseUnits('1', 'ether')
+const INSTANCE_MAX_TICKETS = 100
+const INSTANCE_TIME = "21:00"
+const INSTANCE_DATE = "Sept 8th, 2024"
+const INSTANCE_LOCATION = "Singapore"
+
+describe("MorphPass", () => {
+    let creator, buyer, morphPass
+
+    beforeEach(async () => {
+        [creator, buyer] = await ethers.getSigners()
+
+        const MorphPass = await ethers.getContractFactory("MorphPass")
+        morphPass = await MorphPass.deploy(NAME, SYMBOL)
+
+        const transaction = await morphPass.connect(creator).list(
+            INSTANCE_NAME,
+            INSTANCE_COST,
+            INSTANCE_MAX_TICKETS,
+            INSTANCE_TIME,
+            INSTANCE_DATE,
+            INSTANCE_LOCATION
+        )
+        
+        await transaction.wait()
+    })
+
+    describe("Deployment", () => {
+        it("Sets Name", async() => {
+            expect(await morphPass.name()).to.equal(NAME)
+        })
+
+        it("Sets Symbol", async () => {
+            expect(await morphPass.symbol()).to.equal(SYMBOL)
+        })
+
+        it("Sets Owner", async () => {
+            expect(await morphPass.owner()).to.equal(creator.address)
+        })
+    })
+
+    describe("Instances", () => {
+        it("Updates Instances Count", async () => {
+            const totalInstances = await morphPass.totalInstances()
+            expect(totalInstances).to.equal(1)
+        })
+
+        it("Returns Instance Attributes", async () => {
+            const instance = await morphPass.getInstance(1)
+            expect(instance.id).to.equal(1)
+            expect(instance.name).to.equal(INSTANCE_NAME)
+            expect(instance.cost).to.equal(INSTANCE_COST)
+            expect(instance.tickets).to.equal(INSTANCE_MAX_TICKETS)
+            expect(instance.time).to.equal(INSTANCE_TIME)
+            expect(instance.date).to.equal(INSTANCE_DATE)
+            expect(instance.location).to.equal(INSTANCE_LOCATION)
+        })
+    })
 })
