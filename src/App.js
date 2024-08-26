b82e5a667a46c07d292bd2c8da5594812890a14d
NaniSkinner <skinner.galvez@gmail.com>
2024-08-26 15:04:22 -0500

    fixed dependencies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './components/Navigation'
-import Sort from './components/Sort'
-import Card from './components/Card'
-import SeatChart from './components/SeatChart'
+import Navigation from "./components/Navigation";
+import Sort from "./components/Sort";
+import Card from "./components/Card";
+import SeatChart from "./components/SeatChart";
 
 // ABIs
-import MorphPass from './abis/morphPass.json'
+import MorphPass from "./abis/morphPass.json";
 
 // Config
-import config from './config.json'
+import config from "./config.json";
 
 function App() {
   return (
     <div>
       <header>
-
-        <h2 className="header__title"><strong>Welcome to morphPass</strong></h2>
+        <h2 className="header__title">
+          <strong>Welcome to morphPass</strong>
+        </h2>
       </header>
-
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,11 @@
-import { ethers } from 'ethers'
+import { ethers } from "ethers";
 
 const Navigation = ({ account, setAccount }) => {
-
   return (
     <nav>
-
+      <h1>Welcome to the Shit Show</h1>
     </nav>
   );
-}
+};
 
 export default Navigation;
